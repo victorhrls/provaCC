5212e74452bcf17f99553b698da38ccf90c05bce
victor Hugo <victorhrls180@gmail.com>
2022-01-12 11:46:17 -0300

    Novas Tentativas

--- Disciplina MC202/Laboratorios/Lab1.c
+++ Disciplina MC202/Laboratorios/Lab1.c
@@ -8,31 +8,31 @@ Faça um programa que calcule a soma da diagonal principal , secundaria e quanta
 #include <stdio.h>
 #define MAX 100
 
-void soma_diagonal(int **matriz, int tamanho,int *principal, int *secundaria){
+void soma_diagonal(int A[MAX][MAX], int tamanho,int **principal, int **secundaria){
     int i;
 
     for(i=0;i<tamanho;i++){ 
-        principal += matriz[i][i];
-        secundaria += matriz[i][tamanho-i];
+        principal += A[i][i];
+        secundaria += A[i][tamanho-i];
     }
 }
 
-void linhas_colunas(int **matriz,int tamanho,int *lines,int *columns){
+void linhas_colunas(int A[MAX][MAX],int tamanho,int **lines,int **columns){
     int i,j,colunas_par,linhas_impar;
 
     // colunas
     for(i=0;i<tamanho;i++){
         for(j=0;j<tamanho;j++){
-            colunas_par += matriz[i][j];
+            colunas_par += A[i][j];
             if (colunas_par%2==0){
-                columns =+1;
+                columns +=1;
             }
         }
     }
     // linhas
     for(j=0;j<tamanho;j++){
         for(i=0;i<tamanho;i++){
-            linhas_impar += matriz[i][j];
+            linhas_impar += A[i][j];
             if(linhas_impar%2!=0){
                 lines +=1;
             }
@@ -43,18 +43,28 @@ void linhas_colunas(int **matriz,int tamanho,int *lines,int *columns){
 
 
 int main(){
-    int M[MAX][MAX],N,*diagonal_principal,*diagonal_secundaria,*linhas,*colunas;
+    int M[MAX][MAX],N,*diagonal_principal,*diagonal_secundaria,*linhas,*colunas,i,j;
 
     printf("Digite o tamanho da sua matriz: ");
     scanf("%d",&N);
 
-    soma_diagonal(M,N,&diagonal_principal,&diagonal_secundaria);
+    printf("Digite os elementos da sua matriz: \n");
+
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            printf("Digite o elemento da matriz M%d%d   ",i,j);
+            scanf("%d",&M[i][j]);
+            
+        }
+    }
 
     linhas_colunas(M,N,&linhas,&colunas);
+    soma_diagonal(M,N,&diagonal_principal,&diagonal_secundaria);
 
-    printf("As linhas possuem %d somatorios impar \n",linhas);
-    printf("As colunas possuem %d somatorios par \n", colunas);
-    printf("As somas da diagonal principal e secundaria são : %d e %d ",diagonal_principal,diagonal_secundaria);
+
+    printf("As linhas possuem %ls somatorios impar \n",linhas);
+    printf("As colunas possuem %ls somatorios par \n", colunas);
+    printf("As somas da diagonal principal e secundaria são : %ls e %ls ",diagonal_principal,diagonal_secundaria);
 
 
     return 0;
