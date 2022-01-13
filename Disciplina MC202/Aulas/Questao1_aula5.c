bc178c440b66d2d5bb9edf0536a14da8eaba886a
victor Hugo <victorhrls180@gmail.com>
2022-01-13 19:10:54 -0300

    teste primeira questao

---
+++ Disciplina MC202/Aulas/Questao1_aula5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Declare uma struct que armazena as informações de notas de uma turma. Essa estrutura deve armazenar o numero de alunos , as notas das provas 
+e a maior nota!
+
+
+*/
+typedef struct _turma{  
+    double notas,maior_nota;
+    int num_alunos;
+
+}turma;
+
+
+int main(){
+    turma *p;
+    int i,n,pos;
+    double nota_alta=0.0;
+
+    printf("Digite a quantidade de alunos: ");
+    scanf("%d",&n);
+
+    p = malloc ( n * sizeof(turma)); 
+
+    if (p == NULL)
+        printf("Não há memoria o suficiente! \n");
+        exit(1);
+    // malloc para alocar dinamicamente a memoria de 1 turma com n alunos!
+    for (i=0;i<n;i++)
+        scanf("%lf",&p[i].notas);
+
+        if (p[i].notas > nota_alta)
+            nota_alta =p[i].notas;
+            p[i].maior_nota = nota_alta;
+            pos = i;
+    //for para receber as notas de cada alunos e ver qual a maior nota 
+
+    
+    printf("O numero de alunos é %d e a maior nota é %lf",n,p[pos].maior_nota);
+
+    free (p);
+
+
+    return 0;
+}
